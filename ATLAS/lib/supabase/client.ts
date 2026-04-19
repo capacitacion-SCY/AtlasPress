b4999a194ae78f0877ae922938dfc06f23fa7dd3
capacitacion-SCY <capacitacion1050@gmail.com>
2026-04-19 02:46:08 -0300

    Delete ATLAS directory

--- ATLAS/lib/supabase/client.ts
+++
@@ -1,7 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { assertSupabaseEnv } from "./env";
-
-export function createClient() {
-  const env = assertSupabaseEnv();
-  return createBrowserClient(env.url, env.publishableKey);
-}
